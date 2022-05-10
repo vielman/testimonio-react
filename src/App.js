3205ec1c6ade4a0308bc137161616013147df145
Vielman Paredes <vparedes@intelix.biz>
2022-05-10 17:26:48 -0400

    listo el tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-primcipal'>
+        <h1>Here is what our alumni say about:</h1>
+        <Testimonio 
+          nombre='Emma Bostian'
+          pais='Sweden'
+          imagen='emma'
+          cargo='Software Engineer '
+          empresa='Spotify'
+          testimonio='I ve always struggled with learning JavaScript. I ve taken many courses but freeCodeCamps course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify.'
+        />
+        <Testimonio 
+          nombre='Shawn Wang'
+          pais='Singapore'
+          imagen='shawn'
+          cargo='Software Engineer'
+          empresa='Amazon'
+          testimonio='It s scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life.'
+        />
+
+        <Testimonio 
+          nombre='Sarah Chima'
+          pais='Nigeria'
+          imagen='sarah'
+          cargo='Software Engineer'
+          empresa='ChatDesk'
+          testimonio='freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.'
+        />
+      </div>
     </div>
   );
 }
